5e3acd34d7ff67a8eef846a98a243a8f0ccaeb0b
Olaoluwa <contact@afolabiolaoluwa.com>
2016-10-10 15:52:18 +0100

    Deploy to GitHub-Travis-Heroku Build fixed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+require 'devise/orm/active_record'
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -28,7 +29,7 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/active_record'
+
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
