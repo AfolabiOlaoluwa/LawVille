84ce9d8b258009d8adc8cfe759b7b1ea37c27ca5
Olaoluwa <contact@afolabiolaoluwa.com>
2016-10-11 14:54:57 +0100

    First Commit

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
+require 'travis'
+require 'nokogiri'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
