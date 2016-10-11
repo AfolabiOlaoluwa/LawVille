414d415ebefbdb01aaa5c1406d5a8a922140a5e8
Olaoluwa <contact@afolabiolaoluwa.com>
2016-10-11 20:28:35 +0100

    Edited application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require 'carrierwave'
 require 'simple_form'
 require 'social-share-button'
 require 'redactor-rails'
+require 'devise'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
