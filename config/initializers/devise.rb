640c91a16462545571facb9da74c7b8e44c522f2
Olaoluwa <contact@afolabiolaoluwa.com>
2016-10-11 21:17:47 +0100

    Initial Commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,9 +6,9 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  #config.secret_key = '#{SecureRandom.hex(64)}' if Rails.env.production?
+  #config.secret_key = "#{SecureRandom.hex(64)}" if Rails.env.production?
   #config.secret_key = Figaro.env.devise_secret_key if Rails.env.production?
-  config.secret_key = ENV['SECRET_KEY_BASE']
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
 
   # ==> Mailer Configuration
